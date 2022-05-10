d1c7dccd94a8e5c727639c76e1e54814e2f99162
William Mason <93101691+WilliamMaso@users.noreply.github.com>
2022-05-10 10:46:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -85,6 +85,8 @@ checkAuthenticated = (req, res, next) => {
 //_______________________________________________________________________________________________________________________________________________
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + "/views")
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
